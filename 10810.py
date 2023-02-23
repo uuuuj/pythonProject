feada8f0f757f8563f23b21ecf9002346e72dbdf
YU_GALAXY_BOOK\yjth4 <yjth4436@naver.com>
2023-02-23 16:57:15 +0900

    fail ver

--- 10810.py
+++ 10810.py
@@ -4,9 +4,21 @@ j = [0 for num in range(M)]
 k = [0 for num in range(M)]
 for num in range(M):
     i[num], j[num], k[num] = map(int, input().split())
+for num in range(M):
+    i[num] = i[num]-1
+    #j[num] = j[num] - 1
 basket = [0 for num in range(N)]
 n = 0
-for i[n] in range(j[n]):
-    basket[i[n]] = k[n]
-print(basket)
+print("i = ", i)
+print("j = ", j)
+print("k = ", k)
+#크게 반복 네번하고
+def save():
+
+for num in range(M):
+    print(basket)
+    while n == j[n]:
+        basket[n] = k[n]
+    n = n + 1
+print("last ", basket)
 

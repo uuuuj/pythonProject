bbc8959b19db9b586866c9b455623add18242b1c
YU_GALAXY_BOOK\yjth4 <yjth4436@naver.com>
2023-02-23 15:47:07 +0900

    2nd

--- 10810.py
+++ 10810.py
@@ -4,13 +4,9 @@ j = [0 for num in range(M)]
 k = [0 for num in range(M)]
 for num in range(M):
     i[num], j[num], k[num] = map(int, input().split())
-print(i)
-print(j)
-print(k)
-breakpoint()
-i, j, k = map(int, input().split())
 basket = [0 for num in range(N)]
-
-for i in range(j):
-    basket[i] = k
+n = 0
+for i[n] in range(j[n]):
+    basket[i[n]] = k[n]
+print(basket)
 

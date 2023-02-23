8e206f50faa4281c56fecbdef167c90659a1ddd3
YU_GALAXY_BOOK\yjth4 <yjth4436@naver.com>
2023-02-23 16:57:54 +0900

    success ver(feat. google)

--- 10810.py
+++ 10810.py
@@ -1,24 +1,13 @@
 N, M = map(int, input().split())
-i = [0 for num in range(M)]
-j = [0 for num in range(M)]
-k = [0 for num in range(M)]
-for num in range(M):
-    i[num], j[num], k[num] = map(int, input().split())
-for num in range(M):
-    i[num] = i[num]-1
-    #j[num] = j[num] - 1
-basket = [0 for num in range(N)]
-n = 0
-print("i = ", i)
-print("j = ", j)
-print("k = ", k)
-#크게 반복 네번하고
-def save():
+basket = [0] * (N+1)
 
-for num in range(M):
-    print(basket)
-    while n == j[n]:
-        basket[n] = k[n]
-    n = n + 1
-print("last ", basket)
+for _ in range(M):
+    i, j, k = map(int, input().split())
+    for n in range(i, j+1):
+        basket[n] = k
+
+for i in range(1, N+1):
+    print(basket[i], end = ' ')
+
+
 

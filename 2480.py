402b95f5f77274b50a175c1ffbda58b724d466db
YU_GALAXY_BOOK\yjth4 <yjth4436@naver.com>
2023-02-23 18:30:22 +0900

    success but google...

--- 2480.py
+++ 2480.py
@@ -1,29 +1,13 @@
-Dice1, Dice2, Dice3 = map(int, input().split())
+a, b, c = map(int, input().split())
 
-if Dice1 == Dice2:
-    if Dice2 == Dice3:
-        reward = 10000 + (Dice1 * 1000)
-        print(reward)
-    elif Dice2 != Dice3:
-        reward = 1000 + (Dice1 * 100)
-        print(reward)
+if a==b==c:
+    print(10000 + (a*1000))
+elif a==b:
+    print(1000 + (a * 100))
+elif a==c:
+    print(1000 + (a*100))
+elif b==c:
+    print(1000 + (b*100))
+else:
+    print(100+max(a,b,c))
 
-elif Dice1 != Dice2:
-    if Dice1 == Dice3:
-        reward = 1000 + (Dice1 * 100)
-        print(reward)
-    elif Dice2 == Dice3:
-        reward = 1000 + (Dice2 * 100)
-        print(reward)
-    else:
-        if Dice1 > Dice2:
-            if Dice2 > Dice3 or Dice2 < Dice3:
-                reward = Dice1 * 100
-                print(reward)
-        elif Dice1 < Dice2:
-            if Dice2 > Dice3 or Dice2 < Dice3:
-                reward = Dice2 * 100
-                print(reward)
-        elif Dice3 > Dice1:
-            if Dice3 > Dice2 or Dice3 < Dice2:
-                reward = Dice3 * 100
